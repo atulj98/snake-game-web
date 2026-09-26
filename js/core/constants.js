21ec468b3e5072a6c447ef6b091a271eb645de0c
Atul Kumar <atul.j98@gmail.com>
2026-09-26 20:05:21 +0530

    My commit 4

--- js/core/constants.js
+++ js/core/constants.js
@@ -7,8 +7,8 @@ const FPS = 10;
 
 const BACKGROUND_COLOR = "#141414";
 
-const SNAKE_COLOR = "##13F00C";
+const SNAKE_COLOR = "#32CD32";
 
 const SNAKE_HEAD_COLOR = "#70E070";
 
-const FOOD_COLOR = "#EBDC0E";
+const FOOD_COLOR = "#DC3232";
