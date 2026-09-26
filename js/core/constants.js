003b82d2913425da2a1b32fb81cad14e9038050e
Atul Kumar <atul.j98@gmail.com>
2026-09-26 18:07:11 +0530

    My commit 4

--- js/core/constants.js
+++ js/core/constants.js
@@ -1,14 +1,14 @@
-const GAME_WIDTH = 800;
-const GAME_HEIGHT = 600;
+const GAME_WIDTH = 700;
+const GAME_HEIGHT = 500;
 
-const CELL_SIZE = 20;
+const CELL_SIZE = 15;
 
 const FPS = 10;
 
 const BACKGROUND_COLOR = "#141414";
 
-const SNAKE_COLOR = "#32CD32";
+const SNAKE_COLOR = "##13F00C";
 
 const SNAKE_HEAD_COLOR = "#70E070";
 
-const FOOD_COLOR = "#DC3232";
+const FOOD_COLOR = "#EBDC0E";
